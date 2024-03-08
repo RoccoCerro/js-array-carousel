1ade715c61517db2cdabe96d813147450518b465
RoccoCerro <rcerro02@gmail.com>
2024-03-08 17:18:25 +0100

    ADD struttura html dinamica dei thumbnails

--- Bonus2/js/app.js
+++ Bonus2/js/app.js
@@ -1,34 +1,3 @@
-// MILESTONE 2
-// Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array 
-// fornito e un semplice ciclo for che concatena un template literal.
-// Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
-// Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente
-//  attraverso JavaScript.
-
-// PSEUDOCODICE MILESTONE2
-// Creo l'arrey con i nomi delle img
-// Creo un ciclo per prendere uno alla volta i nomi nell'arrey e inserire del codice html per ognuno di essi
-
-// MILESTONE 3
-// Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
-
-// BONUS 1:
-// Aggiungere il ciclo infinito del carosello. Ovvero se è attiva la prima immagine e l'utente clicca la freccia per andare 
-// all’immagine precedente, dovrà comparire l’ultima immagine dell’array e viceversa.
-
-// BONUS 2:
-// Aggiungere la visualizzazione di tutte le thumbnails sulla destra dell’immagine grande attiva, come nello screenshot proposto. 
-// Tutte le miniature avranno un layer di opacità scura, tranne quella corrispondente all’immagine attiva, che invece avrà un bordo colorato.
-// Al click delle frecce, oltre al cambio di immagine attiva, gestire il cambio di miniatura attiva.
-
-// PSEUDOCODICE BONUS2
-// - Aggiungere la visualizzazione di tutte le thumbnails sulla destra dell’immagine grande attiva, come nello screenshot proposto.
-//  - Creerò un container nell'html in cui andro ad inserire tutte le miniature in maniera dinamica
-//    - Con position lo posiziono come nella foto
-// - Tutte le miniature avranno un layer di opacità scura, tranne quella corrispondente all’immagine attiva, che invece avrà un bordo colorato.
-//  - Come con l'esercizio precendete andro ad aggiungere e togliere le classi per far schiarire l'img
-
-
 const arrayImg = [
     "./img/01.webp",
     "./img/02.webp",
@@ -41,9 +10,11 @@ const containerItemsDomElement = document.querySelector(".items-container");
 const nextDomElement = document.querySelector('.next');
 const backDomElement = document.querySelector('.back');
 let divHtmlImg = ``;
+let divHtmlThumbnails = "";
 
 let currentIndex = 0;
 let itemElement = document.getElementsByClassName("item");
+let containerItemsThumbnailsElement = document.querySelector(".items-thumbnails");
 
 for (let i = 0; i < arrayImg.length; i++) {
     let img = arrayImg[i];
@@ -56,8 +27,16 @@ for (let i = 0; i < arrayImg.length; i++) {
             <img src="${img}"></img>
         </div>
     `;
+    divHtmlThumbnails = `
+        <div class="col">
+            <img src="${img}"></img>
+        </div>
+    `
+
     containerItemsDomElement.innerHTML += divHtmlImg;
+    containerItemsThumbnailsElement.innerHTML += divHtmlThumbnails;
 }
+
 
 nextDomElement.addEventListener("click", function(){
 
